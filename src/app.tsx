c417a3a32d7b298b70287b8c6ae0c0228c2d7a77
Roger Andersen <hexjelly@hexjelly.com>
2024-09-17 13:31:39 +0300

    fix: ensure min height

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,7 @@ import { UserList } from "./features/user-list";
 
 function App() {
 	return (
-		<main>
+		<main className="bg-ds-bg h-full flex-1 flex items-center justify-center">
 			<UserList />
 		</main>
 	);
