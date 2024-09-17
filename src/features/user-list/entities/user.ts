3eba67219d54c6df0b416b7a7b7ab964a6fc3146
Roger Andersen <hexjelly@hexjelly.com>
2024-09-17 21:27:56 +0300

    fix: type roles

--- src/features/user-list/entities/user.ts
+++ src/features/user-list/entities/user.ts
@@ -1,7 +1,9 @@
+export type Role = "ACCOUNT_MANAGER" | "ADMIN" | "AGENT" | "EXTERNAL_REVIEWER";
+
 export type User = {
 	avatar: string;
 	email: string;
 	id: number;
 	name: string;
-	role: string;
+	role: Role;
 };
