100845ab8e1cce433924311274f3e3a27754fc7a
Roger Andersen <hexjelly@hexjelly.com>
2024-09-17 21:25:53 +0300

    feat: tanstack query dev tools

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./app.tsx";
 import "./index.css";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClient } from "./config/query-client.ts";
 import { enableMocking } from "./mocks/helpers.ts";
 
@@ -16,6 +17,7 @@ enableMocking().then(() => {
 	createRoot(root).render(
 		<StrictMode>
 			<QueryClientProvider client={queryClient}>
+				<ReactQueryDevtools initialIsOpen={false} />
 				<App />
 			</QueryClientProvider>
 		</StrictMode>,
