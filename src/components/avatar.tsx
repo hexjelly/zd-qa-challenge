2f2be6e38094c4665195ab094a6ff0e2ece07c5b
Roger Andersen <hexjelly@hexjelly.com>
2024-09-18 13:26:13 +0300

    fix: placeholder user icon

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -7,7 +7,14 @@ export type AvatarProps = {
 export function Avatar({ email, image, name }: AvatarProps) {
 	return (
 		<div className="flex items-center gap-3">
-			<img className="w-8 h-8 rounded-full" src={image} alt={name} />
+			<img
+				className="w-8 h-8 rounded-full"
+				src={image}
+				alt={name}
+				onError={(e) => {
+					(e.target as HTMLImageElement).src = "/placeholder-avatar.svg";
+				}}
+			/>
 			<div className="text-sm">
 				<div className="font-medium">{name}</div>
 				<div className="text-sm text-ds-subtle">{email}</div>
