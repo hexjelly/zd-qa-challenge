b02bec275ccc426285184ed8ac7427881dfb1997
Roger Andersen <hexjelly@hexjelly.com>
2024-09-18 12:57:11 +0300

    feat: custom checkbox

--- src/components/checkbox.tsx
+++ src/components/checkbox.tsx
@@ -1,4 +1,7 @@
 import { type HTMLProps, useEffect, useRef } from "react";
+import CheckIcon from "../icons/check.svg?react";
+import IndeterminateIcon from "../icons/indeterminate.svg?react";
+import { twMerge } from "tailwind-merge";
 
 export type CheckboxProps = {
 	indeterminate?: boolean;
@@ -13,5 +16,32 @@ export function Checkbox({ indeterminate, checked, ...rest }: CheckboxProps) {
 		}
 	}, [indeterminate, checked]);
 
-	return <input checked={checked} type="checkbox" ref={ref} {...rest} />;
+	return (
+		// biome-ignore lint: too annoying atm
+		<label
+			className={twMerge([
+				"w-4 h-4 bg-white rounded text-white flex items-center justify-center border border-ds-border",
+				checked && "bg-ds-primary",
+				indeterminate && "bg-ds-primary",
+			])}
+			onClick={(e) => e.stopPropagation()}
+		>
+			{indeterminate && (
+				<IndeterminateIcon
+					aria-hidden
+					className="pointer-events-none w-full h-full m-0.5"
+				/>
+			)}
+			{checked && !indeterminate && (
+				<CheckIcon aria-hidden className="pointer-events-none w-full h-full" />
+			)}
+			<input
+				className="appearance-none"
+				checked={checked}
+				type="checkbox"
+				ref={ref}
+				{...rest}
+			/>
+		</label>
+	);
 }
